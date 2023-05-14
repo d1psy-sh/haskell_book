ddf806e470fba1ed8d584efbb2886f8e71afb73f
nkirschall <niki.nice1203@gmail.com>
2023-05-14 21:14:35 +0200

    feat: types

--- html_printer/main.hs
+++ html_printer/main.hs
@@ -1,4 +1,32 @@
-main = putStrLn (wrapHtml "Hello World")
+main = putStrLn myHtml
 
-wrapHtml content = "<html><body>" <> content <> "</body></html>"
+myHtml :: String
+myHtml =
+  makeHtml
+    "Hello, World"
+    (h1_ "Hello, World" <> p1_ "This is my first Haskell program")
 
+head_ :: String -> String
+head_ = el "head"
+
+title_ :: String -> String
+title_ = el "title"
+
+html_ :: String -> String
+html_ = el "html"
+
+body_ :: String -> String
+body_ = el "body"
+
+p1_ :: String -> String
+p1_ = el "p1"
+
+h1_ :: String -> String
+h1_ = el "h1"
+
+el :: String -> String -> String
+el tag content =
+  "<" <> tag <> ">" <> content <> "</" <> tag <> ">"
+
+makeHtml :: String -> String -> String
+makeHtml title content = html_ (head_ (title_ title) <> body_ content)
