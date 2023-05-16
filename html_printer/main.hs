905d2a3c3b7f5dcfb2b39221305ec439717a183b
nkirschall <niki.nice1203@gmail.com>
2023-05-16 18:55:09 +0200

    Added some more code and types to the html_printer

--- html_printer/main.hs
+++ html_printer/main.hs
@@ -1,5 +1,9 @@
 main = putStrLn myHtml
 
+newtype Html = Html String
+
+newtype Structure = Structure String
+
 myHtml :: String
 myHtml =
   makeHtml
@@ -27,6 +31,19 @@ h1_ = el "h1"
 el :: String -> String -> String
 el tag content =
   "<" <> tag <> ">" <> content <> "</" <> tag <> ">"
+
+append_ :: Structure -> Structure -> Structure
+append_ (Structure a) (Structure b) = Structure (a <> b)
+
+render :: Html -> String
+render html =
+  case html of
+    Html str -> str
+
+getStructureString :: Structure -> String
+getStructureString struct =
+  case struct of
+    Structure str -> str
 
 makeHtml :: String -> String -> String
 makeHtml title content = html_ (head_ (title_ title) <> body_ content)
